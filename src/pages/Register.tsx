dd975fbbb5ea86121bf5ba14faab44224b90bdf0
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-28 17:47:21 +0200

    Add Axios configuration to the Register screen

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,20 +1,41 @@
 import "../App.css";
-import { Link } from "react-router-dom";
-
+import { Link , Navigate} from "react-router-dom";
+import { useState, type FormEvent } from "react";
+import api from "../api/axios";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  
+  const handleRegister = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await api.post("/register", { username, password });
+      const {token, userDto} = response.data;
+
+      localStorage.setItem("token", token);
+      
+    } catch (err: any) {
+      setError("Registration failed. Try a different username.");
+    }
+  };
   return (
     <div className="auth-container">
       <div className="auth-card">
         <h1>Create Account</h1>
         <p>Join us and start uploading images</p>
         
-        <form>
+        {error && <p style={{ color: "#FA5C5C", fontWeight: "bold" }}>{error}</p>}
+        <form onSubmit={handleRegister}>
           <div className="form-group">
-            <input className="input-field" type="email" placeholder="Email Address" />
+            <input className="input-field" type="email" placeholder="Email Address" value={username}
+              onChange={(e) => setUsername(e.target.value)} />
           </div>
           <div className="form-group">
-            <input className="input-field" type="password" placeholder="Create Password" />
+            <input className="input-field" type="password" placeholder="Create Password" value={password}
+              onChange={(e) => setPassword(e.target.value)}/>
           </div>
           <button type="submit" className="auth-button">Register</button>
         </form>
